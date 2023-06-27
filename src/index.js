3603ead43d85be52165f7a7d50a1ee7b60c5d961
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-27 20:16:32 +0330

    Crating the Stars

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating  />
+     
   </React.StrictMode>
 );
