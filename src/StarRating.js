3a3535ed8b8a7acf31b3eafed1001b422a033e83
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-28 13:17:23 +0330

    PropTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 const containertStyle = {
   display: "flex",
@@ -9,6 +10,16 @@ const containertStyle = {
 const starContainerStyle = {
   display: "flex",
 };
+
+
+StarRating.propTypes = {
+  maxRating : PropTypes.number ,
+  defaultRating : PropTypes.number ,
+  color : PropTypes.string ,
+  size : PropTypes.array ,
+  className : PropTypes.string,
+  onSetRating : PropTypes.func
+}
 
 export default function StarRating({
   maxRating = 5,
