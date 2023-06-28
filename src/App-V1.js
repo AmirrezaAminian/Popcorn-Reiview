1a7e1ecc07e8463eab9b1d292a64a43f47695c44
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-28 15:54:42 +0330

    3. How NOT to Fetch Data in React

--- src/App-V1.js
+++ src/App-V1.js
@@ -50,16 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
   return (
     <>
       <NavBar>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test(){
-  return <StarRating color="blue" maxRating={10} />
-}
+// function Test(){
+//   return <StarRating color="blue" maxRating={10} />
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-     <Test />
+   <App />
   </React.StrictMode>
 );
